acb0b08447c5f43e3da1d9ec1fa79b82b4dc0c1b
Kyler Li <loukylor@gmail.com>
2023-08-29 18:34:38 -0700

    disconnect socketio client on window close

--- components/ClientProvider.tsx
+++ components/ClientProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { createContext, useEffect } from "react";
 import { Client } from "@/lib/api/client";
-import { createContext } from "react";
 import suspend from "@/lib/suspend";
 
 export const ClientContext = createContext<Client | undefined>(undefined);
@@ -12,6 +12,15 @@ export default function ClientProvider({
   children: React.ReactNode
 }) {
   const client = suspend(() => Client.connect(), []);
+
+  useEffect(() => {
+    window.addEventListener("beforeunload", () => client.disconnect());
+
+    return () => {
+      client.disconnect();
+      window.removeEventListener("beforeunload", () => client.disconnect());
+    };
+  }, [client]);
 
   return (
     <ClientContext.Provider value={client}>
