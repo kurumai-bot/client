6ba8281869637e8530a5bcae570356ec1b01ff71
Kyler Li <loukylor@gmail.com>
2023-10-21 17:31:16 -0700

    add jank mobile support

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,16 +8,16 @@ import ModelLoader from "@/components/ModelHandler";
 export default function Page() {
   return (
     <div className="main-wrapper">
-      <div className="h-full p-20">
-        <main className="flex h-full border border-[color:rgb(var(--secondary-rgb))] rounded-lg">
-          <div className="shrink-0 grow-0 basis-[60%] border-r border-[color:rgb(var(--secondary-rgb))]">
+      <div className="h-full p-5 lg:p-20">
+        <main className="lg:flex lg:flex-row-reverse lg:h-full border border-[color:rgb(var(--secondary-rgb))] rounded-lg">
+          <div className="flex min-w-0 max-h-[90svh] flex-1 flex-col p-2.5 lg:p-10">
+            <ChatMessages />
+            <ChatInput />
+          </div>
+          <div className="shrink-0 grow-0 basis-[60%] border-t lg:border-t-0 lg:border-r border-[color:rgb(var(--secondary-rgb))]">
             <Suspense fallback={<div className="flex-center"><LoadingSpinner radius={60} strokeWidth={10} /></div>}>
               <ModelLoader />
             </Suspense>
-          </div>
-          <div className="flex min-w-0 flex-1 flex-col p-10">
-            <ChatMessages />
-            <ChatInput />
           </div>
         </main>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Kurumai",
   description: "A Proof of Concept Assistant Demonstrating AI's Capabilities and the Simplicity in Implementation",
+  // Needed to make media queries in the chrome phone emulator work
+  viewport: "width=device-width, initial-scale=1.0"
 };
 
 export default function RootLayout({
