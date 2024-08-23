ba2ea177d29597daf3413e9096ac4161232f0f3b
Kyler Li <loukylor@gmail.com>
2024-08-22 18:44:03 -0700

    update viewport code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
 import "./globals.css";
+import { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kurumai",
   description: "A Proof of Concept Assistant Demonstrating AI's Capabilities and the Simplicity in Implementation",
-  // Needed to make media queries in the chrome phone emulator work
-  viewport: "width=device-width, initial-scale=1.0"
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0
 };
 
 export default function RootLayout({
